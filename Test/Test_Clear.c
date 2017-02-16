35e2dbaa28999fc0dc1f1f37da93e684d2a8a2b2
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-02-15 22:50:07 -0800

    added more testcases

--- Test/Test_Clear.c
+++ Test/Test_Clear.c
@@ -4,7 +4,7 @@
 #include "../List.h"
 int main(){
 	FILE* out;
-	out = fopen("Test_Append.out", "w");
+	out = fopen("Test_Clear.out", "w");
 	if( out==NULL ){
 		printf("Unable to write to file\n");
 		exit(EXIT_FAILURE);
@@ -12,9 +12,24 @@ int main(){
 	List a = newList();
 	append(a, 3);
 	printList(out, a);
-	append(a, 4);
+	clear(a);
 	printList(out, a);
+
+	List b = newList();
+	prepend(b, 6);
+	printList(out, b);
+	prepend(b, 12);
+	printList(out, b);
+	clear(b);
+	printList(out, b);
+
+	List c = newList();
+	append(c, 5);
+	printList(out, a);
+
 	fclose(out);
+	freeList(&b);
 	freeList(&a);
+	freeList(&c);
 	return 0;
 }
--- Test/Test_Prepend.c
+++ Test/Test_Prepend.c
@@ -4,7 +4,7 @@
 #include "../List.h"
 int main(){
 	FILE* out;
-	out = fopen("Test_Append.out", "w");
+	out = fopen("Test_Prepend.out", "w");
 	if( out==NULL ){
 		printf("Unable to write to file\n");
 		exit(EXIT_FAILURE);
@@ -12,9 +12,15 @@ int main(){
 	List a = newList();
 	append(a, 3);
 	printList(out, a);
-	append(a, 4);
+	prepend(a, 4);
 	printList(out, a);
+	List b = newList();
+	prepend(b, 6);
+	printList(out, b);
+	prepend(b, 12);
+	printList(out, b);
 	fclose(out);
+	freeList(&b);
 	freeList(&a);
 	return 0;
 }
