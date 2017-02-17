297a982eeb94ef81b9aa76133b7a8f41b6a2565a
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-02-16 16:23:42 -0800

    changed list insert index test case

--- Test/Test_Insert_Index.c
+++ Test/Test_Insert_Index.c
@@ -3,7 +3,7 @@
 #include "../List.h"
 int main(){
 	FILE* out;
-	out = fopen("Test_Insert.out", "w");
+	out = fopen("Test_Insert_Index.out", "w");
 	if( out==NULL ){
 		printf("Unable to write to file\n");
 		exit(EXIT_FAILURE);
@@ -23,28 +23,28 @@ int main(){
 	fprintf(out, "\n");
 
 	moveFront(a);
-//	fprintf(out, "%d\n", index(a));//0
+	fprintf(out, "%d\n", index(a));//0
 	insertBefore(a, 99);
 	printList(out, a);//99 0 1 4 9 16 
-//	fprintf(out, "%d\n", index(a));//1
+	fprintf(out, "%d\n", index(a));//1
 	insertAfter(a, 100);
 	printList(out, a);//99 0 100 1 4 9 16
-//	fprintf(out, "%d\n", index(a));//1
+	fprintf(out, "%d\n", index(a));//1
 
 	moveBack(b);
-//	fprintf(out, "%d\n", index(b));//4
+	fprintf(out, "%d\n", index(b));//4
 	insertBefore(b, 99);
 	printList(out, b);//32 18 8 2 99 0
-//	fprintf(out, "%d\n", index(b));//5
+	fprintf(out, "%d\n", index(b));//5
 	insertAfter(b, 100);
 	printList(out, b);//32 18 8 2 99 0 100
-//	fprintf(out, "%d\n", index(b));//5
+	fprintf(out, "%d\n", index(b));//5
 
 	moveFront(a);moveNext(a);moveNext(a);
 	insertBefore(a, 49);
 	insertAfter(a, 50);
 	printList(out, a); // 99 0 49 100 50 1 4 9 16
-//	fprintf(out, "%d\n", index(a));//3
+	fprintf(out, "%d\n", index(a));//3
 
 	fclose(out);
 	freeList(&b);
